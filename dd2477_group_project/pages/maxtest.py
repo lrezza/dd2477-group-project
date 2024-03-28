10f01a4514bf0f6572746588ab9351c0f9e33f99
Max Wiktorsson <Maxennnnn@gmail.com>
2024-03-28 17:32:06 +0100

    Started Input form

--- dd2477_group_project/pages/maxtest.py
+++ dd2477_group_project/pages/maxtest.py
@@ -5,6 +5,13 @@ from dd2477_group_project.templates import template
 import reflex as rx
 
 
+class FormState(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.form_data = form_data
+
 @template(route="/maxtest", title="Max Sida")
 def maxtest() -> rx.Component:
     """The dashboard page.
@@ -18,5 +25,28 @@ def maxtest() -> rx.Component:
         rx.text(
             "Frontenden för denna sida ändras här:",
             rx.code(".venv/dd2477_group_project/pages/maxtest.py"),
+        ),
+        rx.form(
+            rx.hstack(
+                rx.vstack(
+                    rx.input(
+                        placeholder="Search Topic",
+                        name="phrase",
+                    ),
+                    rx.hstack(
+                        rx.text("Some specific toggle:"),
+                        rx.hstack(
+                            rx.switch("Switched", name="switch"),
+                        ),
+                    ),
+                ),
+                rx.button("Submit", type="submit"),
+            ),
+            on_submit=FormState.handle_submit,
+            reset_on_submit=True,
+        ),
+            rx.divider(),
+            rx.heading("Results"),
+            rx.text(FormState.form_data.to_string()
         ),
     )
