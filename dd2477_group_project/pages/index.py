1addb7ea42fd59e2807d0982d859f2140fa4e1b9
Max Wiktorsson <Maxennnnn@gmail.com>
2024-04-03 21:14:37 +0200

    Changed front page to Index
    -Added Maxtest page to Frontend
    -Removed form and FormState from Maxtest

--- dd2477_group_project/pages/index.py
+++ dd2477_group_project/pages/index.py
@@ -5,14 +5,58 @@ from dd2477_group_project.templates import template
 
 import reflex as rx
 
+class FormState(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.form_data = form_data
 
 @template(route="/", title="Podcast Search", image="/github.svg")
 def index() -> rx.Component:
     """The home page.
-
     Returns:
-        The UI for the home page.
+        The UI for the dashboard page.
     """
-    with open("README.md", encoding="utf-8") as readme:
-        content = readme.read()
-    return rx.markdown(content, component_map=styles.markdown_style)
+    return rx.vstack(
+        rx.heading("Podcast Search", 
+                   class_name="text-green-700 text-center w-full",
+                   size="9"
+                   ),
+        rx.text("Wouldn’t it be great to find podcasts that discuss exactly what you are interested in at the moment? Or even better, to find the exact part of the podcast that discusses your topic of interest?",
+                class_name="pt-4 pb-8 text-xl text-center",
+                ),
+        rx.form(
+            rx.hstack(
+                rx.vstack(
+                    rx.input(
+                        placeholder="Search Topic",
+                        name="phrase",
+                    ),
+                    rx.hstack(
+                        rx.text("Some specific toggle:"),
+                        rx.hstack(
+                            rx.switch("Switched", name="switch"),
+                        ),
+                    ),
+                ),
+                rx.button("Submit", type="submit", class_name="bg-green-700"),
+            ),
+            on_submit=FormState.handle_submit,
+            reset_on_submit=True,
+        ),
+            rx.divider(),
+            rx.heading("Results"),
+            rx.text(FormState.form_data.to_string()
+        ),
+        rx.box(
+            rx.link(
+                rx.text("Docs"),
+                href="https://github.com/lrezza/dd2477-group-project",
+                style=styles.link_style,
+                class_name="",
+            ),
+            class_name="w-[94vw] border-solid border-t-2 fixed bottom-0 py-2",
+        )
+    )
+
--- dd2477_group_project/pages/maxtest.py
+++ dd2477_group_project/pages/maxtest.py
@@ -6,13 +6,6 @@ from dd2477_group_project import styles
 import reflex as rx
 
 
-class FormState(rx.State):
-    form_data: dict = {}
-
-    def handle_submit(self, form_data: dict):
-        """Handle the form submit."""
-        self.form_data = form_data
-
 @template(route="/maxtest", title="Max Sida")
 def maxtest() -> rx.Component:
     """The dashboard page.
@@ -28,29 +21,7 @@ def maxtest() -> rx.Component:
         rx.text("Wouldn’t it be great to find podcasts that discuss exactly what you are interested in at the moment? Or even better, to find the exact part of the podcast that discusses your topic of interest?",
                 class_name="pt-4 pb-8 text-xl text-center",
                 ),
-        rx.form(
-            rx.hstack(
-                rx.vstack(
-                    rx.input(
-                        placeholder="Search Topic",
-                        name="phrase",
-                    ),
-                    rx.hstack(
-                        rx.text("Some specific toggle:"),
-                        rx.hstack(
-                            rx.switch("Switched", name="switch"),
-                        ),
-                    ),
-                ),
-                rx.button("Submit", type="submit", class_name="bg-green-700"),
-            ),
-            on_submit=FormState.handle_submit,
-            reset_on_submit=True,
-        ),
-            rx.divider(),
-            rx.heading("Results"),
-            rx.text(FormState.form_data.to_string()
-        ),
+       
         rx.box(
             rx.link(
                 rx.text("Docs"),
