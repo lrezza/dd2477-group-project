4c031742c8f7ff374c241ab11001fe432a4c4e4d
Max Wiktorsson <Maxennnnn@gmail.com>
2024-04-24 16:53:01 +0200

    Prints out response on screen

--- dd2477_group_project/pages/index.py
+++ dd2477_group_project/pages/index.py
@@ -2,6 +2,7 @@
 from elasticsearch import Elasticsearch
 from dd2477_group_project import styles
 from dd2477_group_project.templates import template
+from dd2477_group_project.components import podcast
 
 import reflex as rx
 
@@ -25,7 +26,8 @@ def connect_to_elastic():
 def search(words):
     es = connect_to_elastic()
     response = query_episodes(words, es)
-    print(response) # Prints out the full response
+    #print(response) # Prints out the full response
+    responsetest = response['hits']['hits']
     first_hit = response['hits']['hits'][0]
     only_text = first_hit.get('fields', {}).get('transcript')
     return only_text
@@ -37,14 +39,38 @@ class FormState(rx.State):
     def handle_submit(self, form_data: dict):
         """Handle the form submit."""
         self.form_data = form_data.get('phrase')
-        print(self.form_data)
+        #print(self.form_data)
         result = search(self.form_data)
         self.handle_result(result)
         #response = query_episodes(self.form_data, self.es)  # Fix: Pass self.es as an argument
         
     def handle_result(self, result):
         self.result = result
+        render_podcasts(result)
 
+"""
+def create_podcastlist(result):
+    print(len(result))
+    podcast_list = [
+        {"heading": heading_list,
+        "transcript": transcript_list}
+    ]
+    for i in range(len(result)):
+        print(result[i].get('fields', {}).get('transcript'))
+    return
+"""
+    
+def render_podcasts(result):
+    #print(result)
+    print(result)
+    return 
+    #podcast("TestHeader", result[0].get('fields', {}).get('transcript'))
+    """
+    return rx.foreach(
+        rx.podcast(item.get('fields', {}).get('transcript')),
+        for item in result
+    )
+    """
 
 @template(route="/", title="Podcast Search")
 def index() -> rx.Component:
@@ -89,11 +115,12 @@ def index() -> rx.Component:
             
         ),
             rx.divider(),
-                rx.heading("Search Results",
+                rx.heading("Search Results", size="7",
                            style={"padding-top": 30, "padding-bottom": 20},
                 ),
             #rx.text()
-            rx.text(FormState.result.to_string()),
+            rx.text(FormState.result[0].to_string(), class_name="w-[80vw] lg:w-[50vw]"),
+            #rx.fragment(*render_podcasts(FormState.result)),
         rx.box(
             rx.link(
                 rx.text("Docs"),
@@ -113,7 +140,7 @@ def index() -> rx.Component:
 
 def query_episodes(word, es):
     # Define the nested query
-    max_response_size = 10000
+    max_response_size = 10
 
     query = {
         "query": {
